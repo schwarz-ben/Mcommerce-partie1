262e05c7e8b4054a997d817a560ca4ff9905b808
Benjamin Schwarz <schwarz.ben@gmail.com>
2019-09-04 09:17:05 +0200

    Price validation: issue ERROR.422 if someone submits a product with a price that is not strictly positive. Upon modification of a product we also check for initial existence of the product and issue a ERROR.404 if this is not the case.

--- src/main/java/com/ecommerce/microcommerce/model/Product.java
+++ src/main/java/com/ecommerce/microcommerce/model/Product.java
@@ -19,7 +19,7 @@ public class Product {
     @Length(min=3, max=20, message = "Nom trop long ou trop court. Et oui messages sont plus stylés que ceux de Spring")
     private String nom;
 
-    @Min(value = 1)
+    // @Min(value = 1) // we don't want to leave this to the generic exception, we want to handle it ourselves
     private int prix;
 
     //information que nous ne souhaitons pas exposer
